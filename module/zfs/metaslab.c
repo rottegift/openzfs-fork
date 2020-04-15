ea520468d42c3d4e93d1049890cdcd0a5a142386
Jorgen Lundman <lundman@lundman.net>
2020-04-15 10:02:56 +0900

    Upstream: Add macOS support
    Add source files to enable macOS support
    Change autoconf/Makefiles to compile
    Prepare zfs-tests for macOS but changes come later
    
    Signed-off-by: Jorgen Lundman <lundman@lundman.net>

--- module/zfs/metaslab.c
+++ module/zfs/metaslab.c
@@ -850,9 +850,14 @@ metaslab_group_create(metaslab_class_t *mc, vdev_t *vd, int allocators)
 		metaslab_group_allocator_t *mga = &mg->mg_allocator[i];
 		zfs_refcount_create_tracked(&mga->mga_alloc_queue_depth);
 	}
-
+#if defined(__APPLE__) && defined(_KERNEL)
+	mg->mg_taskq = taskq_create("metaslab_group_taskq", metaslab_load_pct,
+	    maxclsyspri, 10, INT_MAX, TASKQ_THREADS_CPU_PCT | TASKQ_DYNAMIC
+	    | TASKQ_REALLY_DYNAMIC);
+#else
 	mg->mg_taskq = taskq_create("metaslab_group_taskq", metaslab_load_pct,
 	    maxclsyspri, 10, INT_MAX, TASKQ_THREADS_CPU_PCT | TASKQ_DYNAMIC);
+#endif
 
 	return (mg);
 }
