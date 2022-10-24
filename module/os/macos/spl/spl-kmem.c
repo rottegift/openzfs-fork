a9d9aa4e8a80d510c00d53428adc1f2ba2edf20d
Sean Doran <smd@use.net>
2022-10-24 12:43:22 +0100

    tidy logic

--- module/os/macos/spl/spl-kmem.c
+++ module/os/macos/spl/spl-kmem.c
@@ -5033,14 +5033,12 @@ spl_free_thread()
 		}
 
 		if (spl_enforce_memory_caps != 0) {
-			if (spl_dynamic_memory_cap != 0 &&
-			    spl_dynamic_memory_cap < new_spl_free) {
+			if (spl_dynamic_memory_cap != 0) {
 				const int64_t m = spl_dynamic_memory_cap -
 				    segkmem_total_mem_allocated;
 				if (new_spl_free > m)
 					new_spl_free = m;
-			} else if (spl_manual_memory_cap != 0 &&
-			    spl_manual_memory_cap < new_spl_free) {
+			} else if (spl_manual_memory_cap != 0) {
 				const int64_t m = spl_manual_memory_cap -
 				    segkmem_total_mem_allocated;
 				if (new_spl_free > m)
