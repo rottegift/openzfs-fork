4cee337217b50ca6e80ff1cc3a5251d1d5e533b9
Sean Doran <smd@use.net>
2023-08-01 15:59:14 +0100

    parent abd_subpage_cache with spl_heap_arena not abd_cache
    chasing down
    
    panic(cpu 5 caller 0xfffffe001be399ac):
    vmem_hash_delete(0xfffffe7621414000, fffffe734d44c200,
    65536): bad free (name: abd_subpage_cache, addr, size)
    
    vmem_xfree (in zfs) + 1012
    kmem_magazine_destroy (in zfs) + 132
    kmem_depot_ws_reap (in zfs) + 264
    kmem_cache_reap (in zfs) + 172
    taskq_thread (in zfs) + 820

--- module/os/macos/spl/spl-seg_kmem.c
+++ module/os/macos/spl/spl-seg_kmem.c
@@ -306,7 +306,7 @@ segkmem_abd_init()
 	 */
 
 	abd_subpage_arena = vmem_create("abd_subpage_cache", NULL, 0,
-	    512, vmem_alloc_impl, vmem_free_impl, abd_arena,
+	    512, vmem_alloc_impl, vmem_free_impl, spl_heap_arena,
 	    131072, VM_SLEEP | VMC_NO_QCACHE | VM_FIRSTFIT);
 
 	VERIFY3P(abd_subpage_arena, !=, NULL);
