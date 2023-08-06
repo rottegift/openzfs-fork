00496683cc2cb64b97373f0caeaee7ffbbaf2428
Sean Doran <smd+github@mis.use.net>
2023-08-07 00:51:15 +0100

    Some tidying up of atomics, wmsum as aggsum (#12)
    * make macOS's wmsum like common userland: use aggsum
    
    aggsum will give better lock fanout than the atomics
    
    * Tidy the atomics in macos zfs_context_os.h
    
    Obtain stdatomic.h atomic_..() from os/atomic.h, which we
    include for the handy os_cast_to_atomic_pointer().
    
    The stdatomic.h versions are conservative with respect to
    the memory model, which could be revisited in the future,
    moving from sequential consistency to acquire and release,
    which will matter only on Apple Silicon.
    
    Verified the assembly outputs (also learned goldbolt armv8
    clang wants -mcpu=apple-m1 to replace runtime environment
    function calls with LDADDAL and similar assembly op codes;
    xcode clang does that "for free", but is not as pretty as
    godbolt).
    
    Since older Mac OS X don't have os/atomic.h, provide the
    required macros.
    Signed-off-by: Jorgen Lundman <lundman@lundman.net>

--- include/os/macos/spl/sys/wmsum.h
+++ include/os/macos/spl/sys/wmsum.h
@@ -29,39 +29,41 @@
 #include <sys/systm.h>
 #include <sys/atomic.h>
 #include <sys/malloc.h>
+#include <sys/aggsum.h>
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
-struct wmsum
-{
-	uint64_t wm_value;
-};
+/*
+ * Repeat what's in userland lib/libspl/include/sys/wmsum.h, as macOS does not
+ * have an API like the FreeBSD counter(9) or Linux's percpu_counter KPI.
+ */
 
-typedef struct wmsum wmsum_t;
+#define	wmsum_t	aggsum_t
 
 static inline void
 wmsum_init(wmsum_t *ws, uint64_t value)
 {
-	ws->wm_value = value;
+	aggsum_init(ws, value);
 }
 
 static inline void
 wmsum_fini(wmsum_t *ws)
 {
+	aggsum_fini(ws);
 }
 
 static inline uint64_t
 wmsum_value(wmsum_t *ws)
 {
-	return (atomic_add_64_nv(&ws->wm_value, 0));
+	return (aggsum_value(ws));
 }
 
 static inline void
 wmsum_add(wmsum_t *ws, int64_t delta)
 {
-	atomic_add_64(&ws->wm_value, delta);
+	aggsum_add(ws, delta);
 }
 
 #ifdef	__cplusplus
