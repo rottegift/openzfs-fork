101599e7d44a87359c87cbead82b89150ca7bb11
Jorgen Lundman <lundman@lundman.net>
2023-11-09 08:20:56 +0900

    Set zp->z_blksz to ashift
    Slowdowns in zfs_log_write() can happen on large
    blocksize devices.
    
    Signed-off-by: Jorgen Lundman <lundman@lundman.net>

--- module/os/macos/zfs/zfs_znode.c
+++ module/os/macos/zfs/zfs_znode.c
@@ -67,6 +67,8 @@
 #include <sys/zfs_sa.h>
 #include <sys/zfs_stat.h>
 
+#include <sys/spa_impl.h> // spa_min_alloc
+
 #include "zfs_prop.h"
 #include "zfs_comutil.h"
 
@@ -621,6 +623,10 @@ zfs_znode_alloc(zfsvfs_t *zfsvfs, dmu_buf_t *db, int blksz,
 	 */
 	zp->z_zfsvfs = zfsvfs;
 	mutex_exit(&zfsvfs->z_znodes_lock);
+
+	/* This makes a large difference on 4096 block devices */
+	if (zfsvfs->z_os->os_spa->spa_min_alloc > zp->z_blksz)
+		zp->z_blksz = zfsvfs->z_os->os_spa->spa_min_alloc;
 
 	return (zp);
 }
