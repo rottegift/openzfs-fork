bf547c0440f25791d7073c3e932186f3639cf7f5
Jorgen Lundman <lundman@lundman.net>
2024-03-13 09:06:29 +0900

    cpuid uses a,b,c,d order.
    wikipedia paragraph referred to model string, which has
    a special case ordering.
    
    This fixes a bug where old intel would incorretly
    assume AES/qmul feature was available when it was not.
    (And vice-verse, but this would just skip asm versions
    and not panic)
    
    Signed-off-by: Jorgen Lundman <lundman@lundman.net>

--- module/os/macos/spl/spl-processor.c
+++ module/os/macos/spl/spl-processor.c
@@ -95,10 +95,9 @@ spl_cpuid_features(void)
 
 	if (first_time == 1) {
 		first_time = 0;
-		// Wikipedia: stored in EAX, EBX, EDX, ECX (in that order).
-		_spl_cpuid(0, a, b, d, c);
+		_spl_cpuid(0, a, b, c, d);
 		if (a >= 1) {
-			_spl_cpuid(1, a, b, d, c);
+			_spl_cpuid(1, a, b, c, d);
 			_spl_cpuid_features = d | (c << 32);
 
 			// GETBV is bit 26 in ECX. Apple defines it as:
@@ -110,7 +109,7 @@ spl_cpuid_features(void)
 		}
 		if (a >= 7) {
 			c = 0;
-			_spl_cpuid(7, a, b, d, c);
+			_spl_cpuid(7, a, b, c, d);
 			_spl_cpuid_features_leaf7 = b | (c << 32);
 		}
 
