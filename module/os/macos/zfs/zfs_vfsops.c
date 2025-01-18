9d70579cbd11dd75e0691f0f21bdab526fd083be
Sean Doran <smd@use.net>
2025-01-18 14:57:43 +0000

    need mutex_init/cv_init in zfsvfs_create_impl() or panic
    in debug, can't use an uninitialized mutex

--- module/os/macos/zfs/zfs_vfsops.c
+++ module/os/macos/zfs/zfs_vfsops.c
@@ -846,6 +846,9 @@ zfsvfs_create_impl(zfsvfs_t **zfvp, zfsvfs_t *zfsvfs, objset_t *os)
 	avl_create(&zfsvfs->z_hardlinks_linkid, hardlinks_compare_linkid,
 	    sizeof (hardlinks_t), offsetof(hardlinks_t, hl_node_linkid));
 
+        mutex_init(&zfsvfs->z_drain_lock, NULL, MUTEX_DEFAULT, NULL);
+        cv_init(&zfsvfs->z_drain_cv, NULL, CV_DEFAULT, NULL);
+
 	error = zfsvfs_init(zfsvfs, os);
 	if (error != 0) {
 		dmu_objset_disown(os, B_TRUE, zfsvfs);
